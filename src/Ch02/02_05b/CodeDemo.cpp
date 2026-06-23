1ce57f8d62fb80e9be031f41e0321331d315322c
Alok Kumar <119598277+Aloksinghsm@users.noreply.github.com>
2026-06-23 04:56:57 +0000

    commit

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,22 @@
 
 #include <iostream>
 
+int a,b = 5;
 int main(){
-    
+    bool flag;
+    a = 6;
+    flag = false;
+    std::cout<<"a ="<<a<<std::endl;
+    std::cout<<"b ="<<b<<std::endl;
+    std::cout<<"Flag ="<<flag<<std::endl;
+
+    flag = true;
+    std::cout<<"Flag ="<<flag<<std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -12,12 +12,20 @@ int main(){
     std::cout << "Initial Zombies Incoming: " << incoming_zombies << std::endl;
 
     // Addition assignment
+    ammo += 10;
+    std::cout<<"Ammo after finding some: "<<ammo<<std::endl;
 
     // Subtraction assignment
+    ammo -= 5;
+    std::cout<<"Ammo after lossing some: "<<ammo<<std::endl;
 
     // Multiplication assignment
+    incoming_zombies *= 2;
+    std::cout<<"Zombies after multiplication: "<<incoming_zombies<<std::endl;
 
     // Division assignment
+    incoming_zombies /= 5;
+    std::cout<<"Zombies after division: "<<incoming_zombies<<std::endl;
 
     // Remainder assignment
 
